24ad084c9a7c276d0dca97a807ae15cbad0b63ab
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-28 21:52:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
 def oxford_comma(array)
-array.join(", ")
-array[-1] = "and #{array[-1]}"
-
+  if array.length == 1
+    array.join
+elsif array.length == 2
+  array.join("and ")
+else array.length > 2
+  array.join(", ")
+  array[-1] = "and #{array[-1]}"
+end
 end
